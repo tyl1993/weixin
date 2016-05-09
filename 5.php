214ecd1afd38ef8ff8f1b9823bb2ca788c26d13d
tyl1993 <2219558113@qq.com>
2016-05-09 14:33:24 +0800

    2

---
+++ 5.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<form action="4.php" method="post"
+      enctype="multipart/form-data">
+    <label for="file">Filename:</label>
+    <input type="file" name="file" id="file" />
+    <br />
+    <input type="submit" name="submit" value="上传" />
+</form>
+
+</body>
+</html>
