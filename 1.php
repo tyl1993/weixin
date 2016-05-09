afebdd9b58e8b476c393bf89f0afee0166d927f3
tyl1993 <2219558113@qq.com>
2016-05-09 14:35:37 +0800

    1

--- 1.php
+++ 1.php
@@ -1,4 +1,13 @@
-<?php
-setcookie("TMCookie",'PHP从基层到实践');
-setcookie("TMCookie",'PHP从基层到实践',time()+60);
-?>
+﻿<html>
+<body>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<form action="4.php" method="post"
+      enctype="multipart/form-data">
+    <label for="file">Filename:</label>
+    <input type="file" name="file" id="file" />
+    <br />
+    <input type="submit" name="submit" value="上传" />
+</form>
+
+</body>
+</html>
